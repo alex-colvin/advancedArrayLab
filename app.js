bd2f53b614cba5c2d28c0b77e8e6a45c1f2ea1cf
alex-colvin <alexcolvin@live.com>
2022-04-25 11:35:21 -0400

    created problemThree function

--- app.js
+++ app.js
@@ -158,6 +158,20 @@ let italianFood = problemTwo();
 console.log('Italian Foods with five servings or greater: ', italianFood)
 //3. Create a function that will return only dishes whose serving id number matches their serving count.
 //Filter
+function problemThree(){
+
+    let results = dishes.filter(function(el){
+        if(el.id == el.servings){
+            return true;
+        }
+        else{
+            return false;
+        }})
+
+    return results;
+}
+let servingMatchesId = problemThree();
+console.log('Dishes whos serving id number matches their serving count: ', servingMatchesId)
 
 //4. Create a function that will return only dishes whose serving count is even.
 //Filter
